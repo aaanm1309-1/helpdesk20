85113ad8f70579431d935f4404c48e8eba08bec1
Adriano Menezes <aaanm1309@gmail.com>
2024-02-01 18:03:43 -0300

    chore: start user-service-api

---
+++ user-service-api/src/test/java/br/com/adrianomenezes/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adrianomenezes.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

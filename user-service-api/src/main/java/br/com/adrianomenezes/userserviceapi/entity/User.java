cd5cbef37b3f7a81a56d0d3234a189ab8cedb6ec
Adriano Menezes <aaanm1309@gmail.com>
2024-02-01 21:02:28 -0300

    feat: add user entity and commons

---
+++ user-service-api/src/main/java/br/com/adrianomenezes/userserviceapi/entity/User.java
@@ -0,0 +1,24 @@
+package br.com.adrianomenezes.userserviceapi.entity;
+
+import br.com.adrianomenezes.models.enums.ProfileEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@With
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profiles;
+}

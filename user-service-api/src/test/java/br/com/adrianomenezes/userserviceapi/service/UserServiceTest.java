fe484d80ca5a50291295856d3e39d2f844fa6140
Adriano Menezes <aaanm1309@gmail.com>
2024-03-14 22:35:26 -0300

    test: add test to find all user service

--- user-service-api/src/test/java/br/com/adrianomenezes/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/adrianomenezes/userserviceapi/service/UserServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,6 +68,26 @@ class UserServiceTest {
 
         verify(repository, times(2)).findById(anyString());
         verify(mapper, never()).fromEntity(any(User.class));
+    }
+
+    @Test
+    void whenCallFindAllThenReturnListOfUserResponse() {
+
+        when(repository.findAll()).thenReturn(
+                List.of(new User(), new User())
+        );
+        when(mapper.fromEntity(any(User.class))).thenReturn(
+                mock(UserResponse.class)
+        );
+
+        final var response = userService.findAll();
+
+        assertNotNull(response);
+        assertEquals(UserResponse.class, response.get(0).getClass());
+        assertEquals(2, response.size());
+
+        verify(repository, times(1)).findAll();
+        verify(mapper, times(2)).fromEntity(any(User.class));
     }
 
 

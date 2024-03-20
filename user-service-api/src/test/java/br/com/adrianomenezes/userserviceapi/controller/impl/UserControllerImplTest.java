b80ef7063a5da3d011cfc51094e8dba2e192a0cc
Adriano Menezes <aaanm1309@gmail.com>
2024-03-20 09:24:55 -0300

    test: add test integration to find by id with not found exception user controller

--- user-service-api/src/test/java/br/com/adrianomenezes/userserviceapi/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/br/com/adrianomenezes/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static br.com.adrianomenezes.userserviceapi.creator.CreatorUtils.generateMock;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,6 +43,21 @@ class UserControllerImplTest {
                 ;
 
         userRepository.deleteById(userId);
+    }
+
+    @Test
+    void testFindByIdWithNotFoundException() throws Exception {
+        final var userId = "123456";
+
+        mockMvc.perform(get("/api/v1/users/{id}", userId))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("User not found. Id: "+ userId+", Type: UserResponse"))
+                .andExpect(jsonPath("$.error").value(NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.path").value("/api/v1/users/"+ userId))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+        ;
+
     }
 
 }

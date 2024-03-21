eadce9d81e52062e841f3d638ba3712fbf606c6b
Adriano Menezes <aaanm1309@gmail.com>
2024-03-21 10:23:04 -0300

    feat: create order service and imovel service

---
+++ imovel-service-api/src/main/java/br/com/adrianomenezes/imovelserviceapi/ImovelServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.adrianomenezes.imovelserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovelServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImovelServiceApiApplication.class, args);
+	}
+
+}

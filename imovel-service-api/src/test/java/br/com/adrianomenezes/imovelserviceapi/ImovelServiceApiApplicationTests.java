f30825d9ffca075de2e68906604aeae1e2bc5f36
Adriano Menezes <aaanm1309@gmail.com>
2024-03-21 10:23:04 -0300

    feat: create order service and imovel service

---
+++ imovel-service-api/src/test/java/br/com/adrianomenezes/imovelserviceapi/ImovelServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adrianomenezes.imovelserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImovelServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

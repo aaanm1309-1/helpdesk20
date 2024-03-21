a6f25bb5eb6e5caaab824cb206f38f67273c6387
Adriano Menezes <aaanm1309@gmail.com>
2024-03-21 20:13:19 -0300

    feat: add database connection and create first structure

--- order-service-api/src/main/java/br/com/adrianomenezes/orderserviceapi/OrderServiceApiApplication.java
+++ order-service-api/src/main/java/br/com/adrianomenezes/orderserviceapi/OrderServiceApiApplication.java
@@ -1,13 +1,18 @@
 package br.com.adrianomenezes.orderserviceapi;
 
+import br.com.adrianomenezes.orderserviceapi.entities.Order;
+import br.com.adrianomenezes.orderserviceapi.repositories.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApiApplication {
+public class OrderServiceApiApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApiApplication.class, args);
 	}
+
 
 }
---
+++ order-service-api/src/main/java/br/com/adrianomenezes/orderserviceapi/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package br.com.adrianomenezes.orderserviceapi.repositories;
+
+import br.com.adrianomenezes.orderserviceapi.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}

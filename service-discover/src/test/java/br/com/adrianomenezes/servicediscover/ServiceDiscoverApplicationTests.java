c994d3644b3b24d109a9405274100ab3af22b320
Adriano Menezes <aaanm1309@gmail.com>
2024-03-25 10:09:01 -0300

    chore: create service-discover

---
+++ service-discover/src/test/java/br/com/adrianomenezes/servicediscover/ServiceDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adrianomenezes.servicediscover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

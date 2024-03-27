f0f685a28facf1464d15b951b299493ea216982c
Adriano Menezes <aaanm1309@gmail.com>
2024-03-26 22:45:26 -0300

    feat: start helpdesk bff project

---
+++ helpdesk-bff/src/test/java/br/com/adrianomenezes/helpdeskbff/HelpdeskBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adrianomenezes.helpdeskbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
